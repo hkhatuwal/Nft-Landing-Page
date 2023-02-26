244db35ea98c54459aeb58d3ea43e3ab50cebcc0
neerajk1208 <neerajk1208@gmail.com>
2023-02-25 19:26:59 -0800

    undo

--- pages/_document.js
+++ pages/_document.js
@@ -98,12 +98,12 @@ export default function Document() {
                     async
                     src="https://www.googletagmanager.com/gtag/js?id=G-NYW1ZJC3Q6"
                 ></script>
-                <script>
+                {/* <script>
 					window.dataLayer = window.dataLayer || [];
-					function gtag(){window.dataLayer.push(arguments)}
+					function gtag(){dataLayer.push(arguments)}
 					gtag('js', new Date());
 					gtag('config', 'G-KGQMHPCCG7');
-				</script>
+				</script> */}
                 <Main />
                 <NextScript />
             </body>
