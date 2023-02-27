3e6614f5f23d5021c0f09e46fa6d19dab483de72
neerajk1208 <neerajk1208@gmail.com>
2023-02-26 21:42:06 -0800

    fix

--- components/Join/Join.tsx
+++ components/Join/Join.tsx
@@ -7,7 +7,7 @@ const Join = () => {
                 <div className="mx-auto max-w-[1095px] text-center">
                     <h2 className="mb-[25px] text-[30px] leading-[38px] md:text-[48px] md:leading-[54px]">
                         Join the Prime community
-                    </h2>
+                        </h2>
                     <p>
                         We're building Prime for the entire DeFi community, and
                         we want you to help build this experience with us. Click
